5d674541cfd430880a9e27f799c0d2b859bdf329
Joshua Pantoja <38227841+Joshua099@users.noreply.github.com>
2019-05-14 12:34:44 -0700

    Removed EDAs, added 3 queries and analysis
    A best-fit scatterplot graph is more than enough EDA.

--- data_analysis.py
+++ data_analysis.py
@@ -16,18 +16,35 @@ engine_stmt = 'mysql+mysqlconnector://%s:%s@%s:3306/%s' % (user, passw, host, da
 
 engine = sqlalchemy.create_engine(engine_stmt)
 
-# EDA on p_currentmmr and kd_ratio
-
-eda_query = """SELECT kd_ratio, p_currentmmr FROM r6_leaderboard_database.training"""
-eda_df = pd.read_sql(eda_query, con=engine)
-
-
 # OLS of kd_ratio on p_currentmmr
-result = sm.ols(formula="p_currentmmr ~ kd_ratio", data=eda_df).fit()
+
+query1 = """SELECT kd_ratio, p_currentmmr FROM r6_leaderboard_database.training"""
+df1 = pd.read_sql(query1, con=engine)
+
+result = sm.ols(formula="p_currentmmr ~ kd_ratio", data=df1).fit()
 
 print(result.params)
-
 print(result.summary())
 
-sns.lmplot(x='kd_ratio', y='p_currentmmr', data=eda_df, height=12)
+sns.lmplot(x='kd_ratio', y='p_currentmmr', data=df1, height=12)
 plt.show()
+
+
+# OLS of kd_ratio + p_headshotacc on p_currentmmr
+
+query2 = """SELECT kd_ratio, p_headshotacc, p_currentmmr FROM r6_leaderboard_database.training"""
+df2 = pd.read_sql(query2, con=engine)
+
+result2 = sm.ols(formula="p_currentmmr ~ kd_ratio + p_headshotacc", data=df2).fit()
+
+print(result2.summary())
+
+
+# OLS of kd_ratio + p_headshotacc + p_skillrating on p_currentmmr
+
+query3 = """SELECT kd_ratio, p_headshotacc, p_skillrating, p_currentmmr FROM r6_leaderboard_database.training"""
+df3 = pd.read_sql(query3, con=engine)
+
+result3 = sm.ols(formula="p_currentmmr ~ kd_ratio + p_headshotacc + p_skillrating", data=df3).fit()
+
+print(result3.summary())
